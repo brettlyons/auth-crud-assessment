c67f7240274f959655afea9222bdc8838a06e2f7
Brett Lyons <blyons@students.naropa.edu>
2015-10-19 09:41:25 -0600

    Added secured-keys for cookie-session

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+var session = require('cookie-session');
+require ('dotenv').load();
 var users = require('./routes/users');
 
 var app = express();
@@ -20,6 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  name: session,
+  keys: [process.env.KEY_1, process.env.KEY_2, process.env.KEY_3]
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
