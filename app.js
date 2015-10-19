53549d5f8b82689b86e2fc5dbf3471fe828d3197
Brett Lyons <blyons@students.naropa.edu>
2015-10-19 14:10:43 -0600

    changed route to use / instead of /user

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', users);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
