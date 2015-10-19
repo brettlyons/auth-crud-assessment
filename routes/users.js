d27929c03ccf736bb52c752049b81978bbf23c78
Brett Lyons <blyons@students.naropa.edu>
2015-10-19 15:43:45 -0600

    fixed this time?

--- routes/users.js
+++ routes/users.js
@@ -64,13 +64,14 @@ router.get('/signin', function (req, res, next) {
 });
 router.post('/signin', function(req, res, next) {
   users.find({loginEmail: req.body.loginEmail}, function(userEntry) {
+    console.log(userEntry);
     if(bcrypt.compareSync(req.body.loginPass, userEntry.loginPass)) {
       req.session.signedIn = true;
       req.session.name = userEntry.loginEmail;
       res.redirect('/students');
     }
     else {
-      res.render('signin', {
+      res.render('/signin', {
         title: 'the sign in page',
         errors: ["Your signon wasn't found in the db, perhaps you should sign up first."] 
       });
